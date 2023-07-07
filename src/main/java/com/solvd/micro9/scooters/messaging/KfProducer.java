9412f417f47fd53573eb6b8af95e003b12e30f16
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-07-07 19:09:39 +0300

    more generic kfproducer

--- src/main/java/com/solvd/micro9/scooters/messaging/KfProducer.java
+++ src/main/java/com/solvd/micro9/scooters/messaging/KfProducer.java
@@ -1,22 +1,7 @@
 package com.solvd.micro9.scooters.messaging;
 
-import com.solvd.micro9.scooters.domain.RentEvent;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
+public interface KfProducer<K, X> {
 
-@Component
-@RequiredArgsConstructor
-public class KfProducer {
-
-    @Value("${spring.kafka.topic}")
-    private String topic;
-
-    private final KafkaTemplate<String, RentEvent> kafkaTemplate;
-
-    public void send(final RentEvent event) {
-        kafkaTemplate.send(topic, event.getId(), event);
-    }
+    void send(X value);
 
 }

d7da398240ebe51a0f0a1254288ed0cecf069e45
Elizaveta Yakovleva <eyakovleva@solvd.com>
2023-07-11 11:32:46 +0300

    Revert "removed checkstyle-suppressions.xml"
    This reverts commit dbde0367daeff110f0fdc6f5b80266def75141ce.

--- src/main/java/com/solvd/micro9/scooters/StartApplication.java
+++ src/main/java/com/solvd/micro9/scooters/StartApplication.java
@@ -6,10 +6,7 @@ import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
 @EnableKafkaStreams
-public final class StartApplication {
-
-    private StartApplication() {
-    }
+public class StartApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(StartApplication.class, args);
